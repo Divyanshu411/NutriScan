a14d40b69394b700a6aaf761ac1b840ed1000c12
Divyanshu R00208962 <divyanshu@mycit.ie>
2024-03-12 11:30:09 +0000

    Scrollbar and page 3 successfully added to the GUI

--- OCR Research/GUI/NutriScan.py
+++ OCR Research/GUI/NutriScan.py
@@ -8,12 +8,25 @@ from tkinterdnd2 import DND_FILES, TkinterDnD
 class NutriScanApp(TkinterDnD.Tk):
     def __init__(self, *args, **kwargs):
         TkinterDnD.Tk.__init__(self, *args, **kwargs)
-        self.geometry("800x700")
+        self.geometry("700x500")
         self.title('NutriScan')
         self.img_file_path = None
 
-        container = tk.Frame(self)
-        container.grid(sticky="nsew")
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(0, weight=1)
+
+        self.canvas = tk.Canvas(self)
+        self.canvas.grid(row=0, column=0, sticky="nsew")
+
+        scrollbar = tk.Scrollbar(self, orient="vertical", command=self.canvas.yview)
+        scrollbar.grid(row=0, column=1, sticky='ns')
+
+        self.canvas.configure(yscrollcommand=scrollbar.set)
+        self.canvas.bind('<Configure>', lambda e: self.canvas.configure(scrollregion=self.canvas.bbox("all")))
+        self.canvas.bind_all("<MouseWheel>",
+                        lambda event: self.canvas.yview_scroll(int(-1 * (event.delta / 120)), "units"))
+
+        container = tk.Frame(self.canvas)
 
         self.frames = {}
 
@@ -23,6 +36,7 @@ class NutriScanApp(TkinterDnD.Tk):
             frame.grid(row=0, column=0, sticky="nsew")
 
         self.show_frame(PageOne)
+        self.canvas.create_window((0, 0), window=container, anchor="nw")
 
     def show_frame(self, cont):
         frame = self.frames[cont]
@@ -30,10 +44,14 @@ class NutriScanApp(TkinterDnD.Tk):
         if cont == PageTwo:
             frame.initialize_crop_app()
 
+        self.update_idletasks()
+        self.canvas.configure(scrollregion=self.canvas.bbox("all"))
+
 
 class PageOne(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
+
         my_font1 = ('times', 18, 'bold')
         Heading = tk.Label(self, text='NutriScan', width=30, font=my_font1)
         Heading.grid(row=0, column=0, columnspan=2)
@@ -72,14 +90,14 @@ class PageOne(tk.Frame):
             filename = filename.strip('{}')
             img = Image.open(filename)
             width, height = img.size
-            width_new = int(width / 2)
-            height_new = int(height / 2)
+            width_new = int(width / 3)
+            height_new = int(height / 3)
             img_resized = img.resize((width_new, height_new))
             img = ImageTk.PhotoImage(img_resized)
             l2 = tk.Label(self, image=img)
             l2.grid(row=3, column=0, columnspan=2)
             controller.img_file_path = filename
-            print(f"Image loaded in PageOne: {filename}")
+            controller.show_frame(PageOne)
 
         next_page_button = tk.Button(self, text='Next', width=20, command=lambda: controller.show_frame(PageTwo))
         next_page_button.grid(row=5, column=0, columnspan=2)
@@ -89,13 +107,10 @@ class PageTwo(tk.Frame):
     def initialize_crop_app(self):
         if self.controller.img_file_path:
             self.crop_app = CropApp(self, self.controller.img_file_path)
-        else:
-            print("Error: No image file path provided.")
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.controller = controller
-
 
         my_font = ('Ariel', 18, 'bold')
         page_2_label_1 = tk.Label(self, text='Page 2', width=30, font=my_font)
@@ -105,8 +120,6 @@ class PageTwo(tk.Frame):
 
         if controller.img_file_path:
             self.crop_app = CropApp(self, controller.img_file_path)
-        else:
-            print("Error: No image file path provided.")
 
         prev_page_button = tk.Button(self, text='Back', width=20, command=lambda: controller.show_frame(PageOne))
         prev_page_button.grid(row=1, column=0, columnspan=2)
