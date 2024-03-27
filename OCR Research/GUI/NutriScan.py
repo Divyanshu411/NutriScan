076b6264d3320474f7407b167cb6efab35dd0286
Divyanshu R00208962 <divyanshu@mycit.ie>
2024-03-27 21:31:49 +0000

    Updated UI with customtkinter for cleanliness and fixed image orientation

--- OCR Research/GUI/NutriScan.py
+++ OCR Research/GUI/NutriScan.py
@@ -2,7 +2,7 @@ import csv
 import os
 import tempfile
 import tkinter as tk
-from tkinter import ttk, TOP, SUNKEN
+from tkinter import ttk
 from tkinter import filedialog
 import pandas as pd
 from PIL import Image, ImageTk, ExifTags
@@ -12,28 +12,23 @@ from tkinter.simpledialog import askstring
 from OCR_extraction import process_image
 from tkinter.filedialog import asksaveasfile
 import sqlite3
+import customtkinter as ctk
+from tkinter import messagebox
+
+ctk.set_appearance_mode("light")
+ctk.set_default_color_theme("dark-blue")
 
 
 class NutriScanApp(TkinterDnD.Tk):
     def __init__(self, *args, **kwargs):
         TkinterDnD.Tk.__init__(self, *args, **kwargs)
-        self.state('zoomed')
+        self.state("zoomed")
         self.title('NutriScan')
         self.img_file_path = None
         self.cropped_image_path = None
 
-        self.canvas = tk.Canvas(self)
-        self.canvas.pack(side="left", fill="both", expand=True)
-
-        scrollbar = tk.Scrollbar(self, orient="vertical", command=self.canvas.yview)
-        scrollbar.pack(side="right", fill="y")
-
-        self.canvas.configure(yscrollcommand=scrollbar.set)
-        self.canvas.bind('<Configure>', lambda e: self.canvas.configure(scrollregion=self.canvas.bbox("all")))
-        self.canvas.bind_all("<MouseWheel>",
-                             lambda event: self.canvas.yview_scroll(int(-1 * (event.delta / 120)), "units"))
-
-        container = tk.Frame(self.canvas)
+        container = ctk.CTkFrame(self)
+        container.pack(fill="both", expand=True)
 
         self.frames = {}
 
@@ -43,7 +38,6 @@ class NutriScanApp(TkinterDnD.Tk):
             frame.pack(side="top", fill="both", expand=True)
 
         self.show_frame(PageOne)
-        self.canvas.create_window((0, 0), window=container, anchor="nw")
 
     def show_frame(self, cont):
         for frame in self.frames.values():
@@ -57,7 +51,6 @@ class NutriScanApp(TkinterDnD.Tk):
             frame.populate_treeview_from_csv()
 
         self.update_idletasks()
-        self.canvas.configure(scrollregion=self.canvas.bbox("all"))
 
         if cont == CropPopup:
             self.popup_page_two()
@@ -71,7 +64,7 @@ class NutriScanApp(TkinterDnD.Tk):
         page_two_frame.pack(fill="both", expand=True)
 
 
-class PageOne(tk.Frame):
+class PageOne(ctk.CTkFrame):
     def open_popup_page_two(self):
         self.controller.popup_page_two()
 
@@ -81,32 +74,38 @@ class PageOne(tk.Frame):
             return
         try:
             self.img = Image.open(cropped_image_path)
-            self.img = self.img.resize((600, 400))
+            frame_width, frame_height = 600, 400
+            width_ratio = frame_width / self.img.width
+            height_ratio = frame_height / self.img.height
+            resize_ratio = min(width_ratio, height_ratio)
+            new_width = int(self.img.width * resize_ratio)
+            new_height = int(self.img.height * resize_ratio)
+            self.img = self.img.resize((new_width, new_height))
             self.img = ImageTk.PhotoImage(self.img)
 
             for widget in self.frame_image.winfo_children():
                 widget.destroy()
 
-            label = tk.Label(self.frame_image, image=self.img)
-            label.pack()
+            label = ctk.CTkLabel(self.frame_image, image=self.img, text="")
+            label.place(x=frame_width / 2 - new_width / 2, y=frame_height / 2 - new_height / 2)
         except Exception as e:
             print("Error:", e)
 
     def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
+        ctk.CTkFrame.__init__(self, parent)
         self.controller = controller
 
-        my_font1 = ('times', 18, 'bold')
-        Heading = tk.Label(self, text='NutriScan', width=30, font=my_font1)
-        Heading.pack()
+        my_font1 = ('Times', 28, 'bold')
+        Heading = ctk.CTkLabel(self, text='NutriScan', width=30, font=my_font1)
+        Heading.pack(padx=10, pady=10)
 
         my_font2 = ('Arial', 12, 'bold')
-        label_1 = tk.Label(self, text='Upload Image', width=30, font=my_font2)
-        label_1.pack()
+        label_2 = ctk.CTkLabel(self, text='Please choose an Image to extract data', width=30, font=my_font2)
+        label_2.pack()
 
-        entry = tk.Text(self, height=3, width=30)
+        entry = ctk.CTkTextbox(self, height=50, width=300)
         entry.configure(state='normal')
-        entry.insert(1.0, "Drag and Drop files here")
+        entry.insert(1.0, "Drag and Drop here")
         entry.configure(state='disabled')
 
         entry.drop_target_register(DND_FILES)
@@ -114,11 +113,11 @@ class PageOne(tk.Frame):
         entry.pack(pady=10)
 
         my_font3 = ('Arial', 12, 'bold')
-        label_2 = tk.Label(self, text='OR', width=30, font=my_font3)
-        label_2.pack(pady=5)
+        label_3 = ctk.CTkLabel(self, text='OR', width=30, font=my_font3)
+        label_3.pack()
 
-        upload_file_button = ttk.Button(self, text='Upload File', width=20, command=lambda: upload_file())
-        upload_file_button.pack()
+        upload_file_button = ctk.CTkButton(self, text='Select File', width=200, command=lambda: upload_file())
+        upload_file_button.pack(pady=10)
 
         def upload_file():
             f_types = [('Image Files', '*.png *.jpg *.jpeg')]
@@ -132,19 +131,21 @@ class PageOne(tk.Frame):
             img = Image.open(filepath)
 
             orientation_fixed = False
-            for orientation in ExifTags.TAGS.keys():
-                if ExifTags.TAGS[orientation] == 'Orientation':
-                    break
-            exif = dict(img._getexif().items())
+            exif = img._getexif()
+            if exif is not None:
+                for orientation in ExifTags.TAGS.keys():
+                    if ExifTags.TAGS[orientation] == 'Orientation':
+                        break
+                exif_dict = dict(exif.items())
 
-            if 'Orientation' in exif:
-                orientation_fixed = True
-                if exif[orientation] == 3:
-                    img = img.rotate(180, expand=True)
-                elif exif[orientation] == 6:
-                    img = img.rotate(270, expand=True)
-                elif exif[orientation] == 8:
-                    img = img.rotate(90, expand=True)
+                if orientation in exif_dict:
+                    orientation_fixed = True
+                    if exif_dict[orientation] == 3:
+                        img = img.rotate(180, expand=True)
+                    elif exif_dict[orientation] == 6:
+                        img = img.rotate(270, expand=True)
+                    elif exif_dict[orientation] == 8:
+                        img = img.rotate(90, expand=True)
 
             frame_width, frame_height = 600, 400
             width_ratio = frame_width / img.width
@@ -165,25 +166,36 @@ class PageOne(tk.Frame):
             img = ImageTk.PhotoImage(img)
             for widget in self.frame_image.winfo_children():
                 widget.destroy()
-            label = tk.Label(self.frame_image, image=img)
-            label.pack()
+            label = ctk.CTkLabel(self.frame_image, image=img, text="")
+            label.place(x=frame_width / 2 - new_width / 2, y=frame_height / 2 - new_height / 2)
+            label.lift()
             controller.img_file_path = img_file_path
             controller.show_frame(PageOne)
 
-        crop_button = ttk.Button(self, text='Crop Image', width=20, command=self.open_crop_popup)
-        crop_button.pack()
+        crop_button = ctk.CTkButton(self, text='Crop Image', width=20, command=self.open_crop_popup)
+        crop_button.pack(pady=10)
 
-        next_page_button = ttk.Button(self, text='Next', width=20, command=lambda: controller.show_frame(PageTwo))
-        next_page_button.pack()
+        self.frame_image = ctk.CTkFrame(self, width=600, height=400)
+        self.frame_image.pack(padx=10)
 
-        self.frame_image = tk.Frame(self, width=600, height=400)
-        self.frame_image.pack(pady=20, padx=10)
+        next_page_button = ctk.CTkButton(self, text='Next', width=130,
+                                         command=lambda: self.controller.show_frame(PageTwo))
+        next_page_button.pack(pady=5)
 
     def open_crop_popup(self):
-        self.controller.popup_page_two()
+        if not self.controller.img_file_path:
+            messagebox.showerror("Error", "Please select an image first.")
+        else:
+            self.controller.popup_page_two()
+
+    def next_page(self):
+        if not self.controller.img_file_path:
+            messagebox.showerror("Error", "Please select an image first.")
+        else:
+            self.controller.show_frame(PageTwo)
 
 
-class CropPopup(tk.Frame):
+class CropPopup(ctk.CTkFrame):
     def initialize_crop_app(self):
         if self.controller.img_file_path:
             self.crop_app = CropApp(self, self.controller.img_file_path, self.on_image_cropped)
@@ -195,7 +207,7 @@ class CropPopup(tk.Frame):
         self.master.destroy()
 
     def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
+        ctk.CTkFrame.__init__(self, parent)
         self.controller = controller
         self.crop_app = None
 
@@ -203,7 +215,7 @@ class CropPopup(tk.Frame):
             self.initialize_crop_app()
 
 
-class PageTwo(tk.Frame):
+class PageTwo(ctk.CTkFrame):
     def initialize_OCR_extraction(self, img_file_path):
         if self.controller.img_file_path:
             self.img_file_path = img_file_path
@@ -239,8 +251,8 @@ class PageTwo(tk.Frame):
         self.img = ImageTk.PhotoImage(self.img)
         for widget in self.frame_image.winfo_children():
             widget.destroy()
-        self.label = tk.Label(self.frame_image, image=self.img)
-        self.label.pack()
+        self.label = ctk.CTkLabel(self.frame_image, image=self.img, text="")
+        self.label.place(x=frame_width / 2 - new_width / 2, y=frame_height / 2 - new_height / 2)
 
     def edit(self):
         selected_item = self.treeview.selection()[0]
@@ -256,40 +268,50 @@ class PageTwo(tk.Frame):
         self.treeview.delete(selected_item)
 
     def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
+        ctk.CTkFrame.__init__(self, parent)
         self.label = None
         self.img = None
         self.img_file_path = None
         self.process_image_result = None
         self.controller = controller
 
-        my_font = ('Arial', 18, 'bold')
-        page_2_label_1 = tk.Label(self, text='NutriScan', width=30, font=my_font)
+        my_font = ('Arial', 28, 'bold')
+        page_2_label_1 = ctk.CTkLabel(self, text='NutriScan', width=30, font=my_font)
         page_2_label_1.pack()
 
-        prev_page_button = ttk.Button(self, text='Back', width=20, command=lambda: controller.show_frame(PageOne))
-        prev_page_button.pack()
+        prev_page_button = ctk.CTkButton(self, text='Back', width=20, command=lambda: controller.show_frame(PageOne))
+        prev_page_button.pack(side="top", pady=10)
 
-        self.frame_image = tk.Frame(self, width=600, height=400)
-        self.frame_image.pack(side="left", pady=20, padx=20)
-
-        treeview_button_frame = ttk.Frame(self)
+        treeview_button_frame = ctk.CTkFrame(self)
         treeview_button_frame.pack(side="right", padx=20, pady=20)
 
-        edit_btn = ttk.Button(treeview_button_frame, text="Edit", command=self.edit)
-        del_btn = ttk.Button(treeview_button_frame, text="Delete", command=self.delete)
+        edit_btn = ctk.CTkButton(treeview_button_frame, text="Edit", command=self.edit)
+        del_btn = ctk.CTkButton(treeview_button_frame, text="Delete", command=self.delete)
         edit_btn.pack(side="top", padx=5, pady=5)
         del_btn.pack(side="top", padx=5, pady=5)
 
-        frame_table = ttk.Frame(self)
-        frame_table.pack(side="top", fill='both', pady=20)
+        self.frame_image = ctk.CTkFrame(self, width=600, height=400)
+        self.frame_image.pack(side="left", padx=20)
+
+        frame_table = ctk.CTkFrame(self)
+        frame_table.pack(side="top", fill='both', pady=30)
+
+        self.style = ttk.Style()
+        self.style.configure("Treeview",
+                             background="#202120",
+                             foreground="white",
+                             fieldbackground="silver")
 
         self.treeview = ttk.Treeview(frame_table, columns=('Column1', 'Column2', 'Column3'), show='headings', height=30)
-        self.treeview.heading('Column1', text='Nutrient')
-        self.treeview.heading('Column2', text='Value (g)')
-        self.treeview.heading('Column3', text='Confidence Score')
+        self.treeview.heading('Column1', text='Nutrient', anchor='center')
+        self.treeview.heading('Column2', text='Value (g)', anchor='center')
+        self.treeview.heading('Column3', text='Confidence Score', anchor='center')
 
-        vsb = ttk.Scrollbar(frame_table, orient="vertical", command=self.treeview.yview)
+        self.treeview.column('Column1', anchor=tk.CENTER)
+        self.treeview.column('Column2', anchor=tk.CENTER)
+        self.treeview.column('Column3', anchor=tk.CENTER)
+
+        vsb = ctk.CTkScrollbar(frame_table, command=self.treeview.yview)
         self.treeview.configure(yscrollcommand=vsb.set)
 
         self.treeview.pack(side="left", fill="both", expand=True)
@@ -346,16 +368,16 @@ class PageTwo(tk.Frame):
 
             conn.close()
 
-        frame_buttons = tk.Frame(self)
+        frame_buttons = ctk.CTkFrame(self)
         frame_buttons.pack(side="top", padx=5, pady=5)
 
-        export_csv_btn = ttk.Button(frame_buttons, text="Export CSV", command=save_csv)
+        export_csv_btn = ctk.CTkButton(frame_buttons, text="Export CSV", command=save_csv)
         export_csv_btn.pack(side="left", padx=5)
 
-        append_csv_btn = ttk.Button(frame_buttons, text="Append CSV", command=append_csv)
+        append_csv_btn = ctk.CTkButton(frame_buttons, text="Append CSV", command=append_csv)
         append_csv_btn.pack(side="left", padx=5)
 
-        export_append_csv_btn = ttk.Button(frame_buttons, text="Export Append CSV", command=export_appended_csv)
+        export_append_csv_btn = ctk.CTkButton(frame_buttons, text="Export Append CSV", command=export_appended_csv)
         export_append_csv_btn.pack(side="left", padx=5)
 
 
